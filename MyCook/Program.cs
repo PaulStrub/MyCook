567d5534b37c4476e1bb34f4864360130bf26e08
Paul <strubpaul90@gmail.com>
2022-03-17 18:39:22 +0100

    Ajoutez des fichiers projet.

---
+++ MyCook/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+System.Xml
+
+namespace MyCook
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            frigo();
+        }
+
+        public static void frigo()
+        {
+            Console.WriteLine("Bienvenu dans ton frigo virtuelle ici tu auras plein a manger mais seulement en virtuelle.\n " +
+                "[attention ce jeu ne te nourris pas n'oublie pas de manger dans la VRAIE VIE");
+            bool finish = false;
+            while (!finish)
+            {
+                ReadText();
+            }
+        }
+
+        public static readText()
+        {
+
+        }
+        
+    }
+}
