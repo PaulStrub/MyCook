dbdb80e00961ce280155a6a6bf8e4142ea9eb706
Paul <strubpaul90@gmail.com>
2022-03-26 22:11:59 +0100

    modification list and add comment on functions

--- MyCook/Program.cs
+++ MyCook/Program.cs
@@ -17,14 +17,78 @@ namespace MyCook
             bool finish = false;
             while (!finish)
             {
-                ReadText();
+                finish = readText();
             }
         }
 
-        public static readText()
+        public static bool readText()
         {
+            string answerC = answer();
+            bool finish = whatAnswer(answerC);
+            return finish;
+        }
 
+        public static string answer()
+        {
+            Console.WriteLine("Donne nous une commande");
+            string answerC = Console.ReadLine();
+
+            //permet de lire un readline et le retourne si il y est détécté une commande.
+            //detecte la première serie de string jusqu'à un espace.
+            //relance la fonction si ce n'est pas une commande.
+
+            return answerC;
+        }
+
+        public static bool whatAnswer(string answer)
+        {
+            //relis la string et la commande puis en fonction de la commande retourne la fonction qui correspond avec le reste de la string 
+        }
+
+        public static bool addIngredient(string answer)
+        {
+            // add x ingredients if détecte un nombre sinon ajout un seul ingredient (try catch)
+        }
+
+        public static bool addRecipes(string answer)
+        {
+            //add un recette avec le /add_recipes namRecipe x ingredient or /add_recipes namRecipe ingredient avec try catch si il peut int.parse sinon prend un elem de chaque 
+        }
+
+        public static bool help(string answer)
+        {
+            //print all commandes possibles 
+        }
+
+        public static bool makeRecipes(string answer)
+        {
+            //créé la recette en enlevant le nombre d'ingrédient de chaque et ajoutant le plat si assez d'ingrédient sinon print chaques ingrédient manquant et le nombre qu'il faut
+        }
+
+        public static bool showIngredient(string answer)
+        {
+            //montre les ingrédients et le nombre de chaque ingrédient 
+        }
+
+        public static bool showRecipes(string answer)
+        {
+            /*montre le nom de chaque recette de la forme suivante : 
+             nomRecette : 
+                -X ingredients
+                -X ingredients*/
+        }
+
+        public static bool showDishes(string answer)
+        {
+            //montre les différents plats que l'on a 
+        }
+
+        public static bool eatDishes()
+        {
+            //consomme un plat
         }
         
+
+
     }
 }
